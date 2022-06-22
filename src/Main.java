a9374f7e111baaff33d342f832360d27b5cf6f3c
Enes Akbulut <107497616+Enes-Akbulut@users.noreply.github.com>
2022-06-22 19:40:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+           //if ile daha kolay yapılsa bile bunu da bilmek büyük bir artıdır.
+        // yapamayan arkadaslara yardimci olurum. enesakbulut1933@gmail.com
+
+        Scanner input = new Scanner(System.in);
+        double tutar, kdvOrani, kdvliFiyat, netKdv, kdvSecimi;
+        System.out.print("URUN TUTARI =  ");
+        tutar = input.nextDouble();
+
+        // kosul 1 tutar buyuk veya esit 1000, dogruysa 8 degılse 18
+        // bu 2 satırda kosulu ayarladık.
+        boolean kosul1 = tutar >= 1000;
+        kdvSecimi = kosul1 ? 8 : 18;
+
+        netKdv = (tutar * kdvSecimi) / 100;
+        kdvliFiyat = tutar + netKdv;
+        kdvOrani = kdvSecimi;
+
+        // YAZDIRMA ZAMANI
+        System.out.println("kdvsiz fiyat= " + tutar);
+        System.out.println("kdvli fiyat= " + kdvliFiyat);
+        System.out.println("kdv= " + netKdv);
+        System.out.println("kdv orani= " + kdvOrani);
     }
 }
